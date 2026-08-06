1bc11e87f18d2ee442990dbe5f5195fa0dd8ee41
Woodoru <woodoru@naver.com>
2026-08-07 00:28:07 +0900

    chore: configure Apps Script web app

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 // Apps Script를 웹 앱으로 배포한 뒤 발급된 /exec URL을 입력하세요.
 // 이 URL은 공통 북마클릿에 포함되므로 비밀값으로 취급하지 않습니다.
-export const WEB_APP_URL = "";
-
+export const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbw0geMzpsI6n_q9AEoDX1dJ4zH24QML1jmBrHSAxLcer-P8u0dl6Fs8JMblBU0VlT0n/exec";
